f9d9b5c64c220320680553f496c23cae69c66e0f
Nevin <nevink01@mountsinai.org>
2020-08-04 16:31:45 -0400

    Create R script for establishing connection to Oracle database and importing desired data. This will be used in compilation script.

---
+++ Oracle-Database-Connection-Import.R
@@ -0,0 +1,41 @@
+# Code for connecting to and importing data from Oracle
+
+# Install and load packages (dplyr must be loaded before dbplyr)
+# install.packages("DBI")
+# install.packages("odbc")
+# install.packages("dplyr")
+# install.packages("dbplyr")
+
+library(DBI)
+library(odbc)
+library(dplyr)
+library(dbplyr)
+
+# Establish connection to Oracle database
+con <- dbConnect(odbc(), 
+                 Driver = "Oracle in OraClient12Home1", 
+                 DBQ = "idf02-scan/IDMREPD",
+                 Schema = "COO_USER",
+                 UID = "COO_USER",
+                 PWD = "CO02O02rPaVc*MqP", 
+                 Port = 1521)
+
+
+# Import IP census for Epic sites
+start1 <- proc.time()
+
+oracle_ip_census_df <- tbl(con, "EMR_ALL_PAT_DAYS_MSX") %>%
+  filter(year(CENSUS_DATE) == 2020) %>%
+  collect()
+
+end1 <- proc.time() - start1
+
+# Import COVID census for Epic sites
+start2 <- proc.time()
+
+oracle_covid_census_df <- tbl(con, "COVID_IP_PATIENT_DAYS_DETAIL") %>%
+  filter(year(CENSUS_DATE) == 2020 &
+           !(IP_SITE %in% c("MSSN", "MSBI", "UNDETERMINED"))) %>%
+  collect()
+
+end2 <- proc.time() - start2
