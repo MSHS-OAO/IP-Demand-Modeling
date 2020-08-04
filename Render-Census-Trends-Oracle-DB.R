3152f0df32f338031c7457cf055c7fdf8cca7f2d
kateenevin <nevink01@mountsinai.org>
2020-08-04 16:36:20 -0400

    Create R script that first references code to connect and import Oracle database and then renders markdown for census visualizations.

---
+++ Render-Census-Trends-Oracle-DB.R
@@ -0,0 +1,14 @@
+# Code to import Oracle database and render markdown
+
+# Clear any current variables
+rm(list = ls())
+
+# Establish connection to database and import raw data
+source(file = "Oracle-Database-Connection-Import.R")
+
+# Render markdown file for visualizing census trends
+rmarkdown::render(input = "Census-Trend-Visualizations.Rmd", 
+                  output_file = paste0(user_directory, 
+                  "//Census Trend Markdown Reporting//IP-Census-Trends ",
+                  Sys.Date()))
+                  
