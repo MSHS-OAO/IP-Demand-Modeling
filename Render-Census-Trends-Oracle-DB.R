6e8bdd031fb0c8c7fa4cb2a58016035ddaf231ef
kateenevin <nevink01@mountsinai.org>
2020-08-24 13:17:01 -0400

    Move user_directory mapping to top level render wrapper file.

--- Render-Census-Trends-Oracle-DB.R
+++ Render-Census-Trends-Oracle-DB.R
@@ -6,7 +6,15 @@ rm(list = ls())
 # Establish connection to database and import raw data
 source(file = "Oracle-Database-Connection-Import.R")
 
+# Determine file path directory based on shared drive mapping
+if (list.files("J://") == "Presidents") {
+  user_directory <- "J:/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/System Operations/COVID Scenario Testing"
+} else {
+  user_directory <- "J:/deans/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/System Operations/COVID Scenario Testing"
+}
+
 # Render markdown file for visualizing census trends
 rmarkdown::render(input = "Census-Trend-Visualizations.Rmd", 
                   output_file = paste0(user_directory, "/Epic Census Rmd Output/IP Census Dashboard ",
                   Sys.Date()))
+
